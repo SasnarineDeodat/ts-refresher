49b9a470878b89cdc9f2f333855b7f11751b7ae2
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-26 14:26:12 -0500

    Working with Object Types

--- first-app.ts
+++ first-app.ts
@@ -13,3 +13,22 @@ let userID: string | number = "abc1";
 userID = 1234;
 
 // userId = true;
+
+// let user: object;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+// user = "Robbie";
+
+user = {
+  name: "Robbie",
+  age: 34,
+  isAdmin: true,
+  id: "abc", // 123
+};
+
+// user = {};
